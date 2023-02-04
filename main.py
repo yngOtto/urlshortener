f859146e6a6caddeb59e29d4c41d28c741a94541
Otto <olarsen22@gmail.com>
2023-02-05 00:55:03 +0100

    add post endpoint

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # shortener_app/main.py
 
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
 
-app = FastAPI()
+from . import schemas
 
-@app.get("/")
-def read_root():
-    return "Welcome to Otto's URL shortener API :)"
+# ...
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+# ...
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
