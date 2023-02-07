356fec6b7eb4e00d5988050c52e53cc2363718e8
Otto <olarsen22@gmail.com>
2023-02-07 19:40:24 +0100

    add redirect response

--- main.py
+++ main.py
@@ -37,3 +37,25 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+# ...
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+    ):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
